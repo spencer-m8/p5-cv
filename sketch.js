a38457729b737bc02da9679075fb8aca320270d4
spencer-m8 <shsmclean@gmail.com>
2026-05-28 15:03:47 -0400

    adding array to track hand pos

--- sketch.js
+++ sketch.js
@@ -18,14 +18,25 @@ function setup() {
   });
 }
 
+let maxHands = 3;
+let handsArray = [];
+handsArray.length = maxHands;
+let numHands = 0;
+
 function draw() {
   background(0);
   for (let hand of hands) {
+    let j = 0;
     for (let kp of hand.keypoints) {
       fill(0, 255, 0);
       noStroke();
       circle(kp.x, kp.y, 10);
+      handsArray[numHands][0][j] = kp.x;
+      handsArray[numHands][1][j] = kp.y;
+      console.log("x: " + handsArray[numHands][0][j]);
+      console.log("y: " + handsArray[numHands][1][j]);
     }
+    numHands++;
   }
 }
 
